29880ea1a4a895896d3992b314431b82e273715a
KatyaEames <katya.eames@gmail.com>
2015-01-17 13:39:07 -0700

    textarea

--- animations.js
+++ animations.js
@@ -1,6 +1,4 @@
 $('.tweet-compose').on('click', function() {
 	$('this').css('height', '4.5em');
-});
-$('.tweet-controls').on('click', function() {
-	$('this').css(visibilty: );
+	$('.tweet-controls').show;
 });
